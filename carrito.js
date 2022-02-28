0d7dca1719ae49be9f728521030597a0b653dfa0
CuervoXP <matibrian05@gmail.com>
2022-02-28 17:36:37 -0300

    footer

--- carrito.js
+++ carrito.js
@@ -1,9 +1,8 @@
 // Obtengo el carrito en objeto
 const contenedorCarrito = document.querySelector('#items2'); // selecciono el div en el que pondre la tabla de productos
 const footer2 = document.querySelector('#footer2');
-const json = JSON.parse(localStorage.getItem("carritoDeCompras"));
+let json = JSON.parse(localStorage.getItem("carritoDeCompras"));
 const templateFooter2 = document.querySelector('#template-footer2');
-
 
 
 
@@ -11,9 +10,25 @@ const templateFooter2 = document.querySelector('#template-footer2');
 document.addEventListener('DOMContentLoaded', () => {
     
     
+})
+document.addEventListener('DOMContentLoaded', () => {
     contenedorCarrito.innerHTML = '';
-    Object.values(json).forEach(producto => {
-    const mostrarCarrito2 = () => {
+
+    if(localStorage.getItem('carritoDeCompras')) {
+        json = JSON.parse(localStorage.getItem('carritoDeCompras')); // el valor de carrito va a pasar de texto plano, a un objeto,
+            Object.values(json).forEach(producto => {
+            mostrarCarrito2(producto)
+            
+            mostrarFooter2();
+        })
+    }
+    
+    
+
+})
+    
+
+const mostrarCarrito2 = (producto) => {
 
         contenedorCarrito.innerHTML += `<tr>
                     
@@ -25,38 +40,39 @@ document.addEventListener('DOMContentLoaded', () => {
         <td>${producto.precio}</td>
         </tr>`
         
+}
+    const mostrarFooter2 = () => {
+        let carrito2 = JSON.parse(localStorage.getItem('carritoDeCompras'));
+
+        footer2.innerHTML = ''
+        const productosTotal = Object.values(carrito2).reduce((cantidadOld, producto) => {
+            return cantidadOld + producto.cantidad;
+        },0 )
+        const precioTotal = Object.values(carrito2).reduce((precioOld, producto) => {
+            return precioOld + producto.precio * producto.cantidad;
+        },0)
+        
+        footer2.innerHTML = `<th>Total productos</th>
+        <td>${productosTotal}</td>
+        <td>
+            <button class="vaciar-todo" id="vaciar-carrito2">
+                vaciar todo
+            </button>
+        </td>
+        <td class="precio-footer2">$ <span>${precioTotal}</span></td>`
+        
+
+        const vaciarCarrito = document.getElementById('vaciar-carrito2');
+        vaciarCarrito.addEventListener('click', () => {
+            Object.values(carrito2).forEach(producto => {
+                producto = {};
+                mostrarCarrito2(producto)
+                
+            })
+            
+        })
+
+        if(Object.values(json).length === 0) {
+            footer2.innerHTML = `<th>Carrito vacío :(</th>`
+        }
     }
-    mostrarCarrito2();
-    // mostrarFooter2();
-})
-
-})
-/*const mostrarFooter2 = () => {
-    footer2.innerHTML = ''
-    const productosTotal = Object.values(json).reduce((cantidadOld, producto) => {
-        return cantidadOld + producto.cantidad;
-    },0 )
-    const precioTotal = Object.values(json).reduce((precioOld, producto) => {
-        return precioOld + producto.precio * producto.cantidad;
-    },0)
-    // Pinto el productoTotal y precioTotal en el template
-
-    templateFooter2.querySelectorAll('td')[0].textContent = productosTotal;
-    templateFooter2.querySelectorAll('td')[2].textContent = precioTotal;
-
-    const clone3 = templateFooter2.cloneNode(true); // Lo clono cada vez que entre en mostrarCarrito()
-    fragment.appendChild(clone3);
-    footer2.appendChild(fragment);
-
-    const vaciarCarrito = document.getElementById('vaciar-carrito2');
-    vaciarCarrito.addEventListener('click', () => {
-        json = {} // :)
-        mostrarCarrito2();
-    })
-
-    if(Object.values(json).length === 0) {
-        footer.innerHTML = `<th>Carrito vacío :(</th>`
-    }
-}*/
-
-
